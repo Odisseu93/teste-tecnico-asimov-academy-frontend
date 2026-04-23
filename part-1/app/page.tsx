0360c059e55e1588df1e74fe2c6b1a7d30941f4a
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2026-04-23 16:43:36 -0300

    style(ui): apply formatting and constraint max width on home page

--- part-1/app/page.tsx
+++ part-1/app/page.tsx
@@ -1,29 +1,29 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import LogoTicker from "@/components/LogoTicker";
-import Services from "@/components/Services";
-import CTABanner from "@/components/CTABanner";
-import CaseStudies from "@/components/CaseStudies";
-import WorkingProcess from "@/components/WorkingProcess";
-import Team from "@/components/Team";
-import Testimonials from "@/components/Testimonials";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Navbar from '@/components/Navbar'
+import Hero from '@/components/Hero'
+import LogoTicker from '@/components/LogoTicker'
+import Services from '@/components/Services'
+import CTABanner from '@/components/CTABanner'
+import CaseStudies from '@/components/CaseStudies'
+import WorkingProcess from '@/components/WorkingProcess'
+import Team from '@/components/Team'
+import Testimonials from '@/components/Testimonials'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-white selection:bg-lime selection:text-dark">
-      <Navbar />
-      <Hero />
-      <LogoTicker />
-      <Services />
-      <CTABanner />
-      <CaseStudies />
-      <WorkingProcess />
-      <Team />
-      <Testimonials />
-      <Contact />
-      <Footer />
-    </main>
-  );
+	return (
+		<main className='min-h-screen max-w-[1600px]  bg-white selection:bg-lime selection:text-dark'>
+			<Navbar />
+			<Hero />
+			<LogoTicker />
+			<Services />
+			<CTABanner />
+			<CaseStudies />
+			<WorkingProcess />
+			<Team />
+			<Testimonials />
+			<Contact />
+			<Footer />
+		</main>
+	)
 }
